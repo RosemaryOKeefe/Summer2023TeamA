f1ccca0b39fa58e8a01d7e74a61c1fbab7341ada
kavthecoder <bansalk1@mca.org.tw>
2023-08-01 15:55:04 -0600

    lift code deleted

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmode/MainOpsMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmode/MainOpsMode.java
@@ -72,9 +72,6 @@ public class MainOpsMode extends LinearOpMode {
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
-    private DcMotor liftDrive = null;
-    private Servo claw = null;
-    private Servo wrist = null;
 
     private static final double SPEED = 1.0;
 
@@ -84,13 +81,9 @@ public class MainOpsMode extends LinearOpMode {
     private static final int JUNCTION_ENCODING_MEDIUM = 4230;
     private static final int JUNCTION_ENCODING_TALL = 6300;
 
-    private static final double CLAW_POSITION_OPEN = 0.4;
-    private static final double CLAW_POSITION_CLOSED = 0.7;
     private static final double WRIST_POSITION_LEVEL = 0.55;
     private static final double WRIST_POSITION_DOWN = 0.2;
-    private static final double LIFT_MOTOR_SPEED = 1.0;
     private static final double DRIVE_SPEED = 0.6;
-    private int liftPosition = 0;
 
     @Override
     public void runOpMode() {
@@ -100,9 +93,6 @@ public class MainOpsMode extends LinearOpMode {
         leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
         rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
-        liftDrive = hardwareMap.get(DcMotor.class, "lift_drive");
-        claw = hardwareMap.get(Servo.class, "claw");
-        wrist = hardwareMap.get(Servo.class, "wrist");
 
         //set directions
         leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -110,27 +100,16 @@ public class MainOpsMode extends LinearOpMode {
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
-        //reset the encoder
-        liftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        liftDrive.setTargetPosition(0);
-        liftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        liftDrive.setPower(SPEED);
-
-        wrist.setPosition(WRIST_POSITION_LEVEL);
-
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         waitForStart();
         runtime.reset();
-        claw.setPosition(CLAW_POSITION_CLOSED);
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
             updateDriveMotors();
-            updateLift();
-            updateClaw();
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.update();
@@ -193,93 +172,5 @@ public class MainOpsMode extends LinearOpMode {
         telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
         telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
     }
-
-    private void updateLift() {
-        //manual control
-        if (Math.abs(gamepad2.left_stick_y) > 0.15) {
-            // if (liftDrive.getCurrentPosition() <= JUNCTION_ENCODING_TALL && liftDrive.getCurrentPosition() >= 0) {
-            liftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            liftDrive.setPower(gamepad2.left_stick_y);
-            // }
-        }
-        //while right trigger is pressed, do manual control
-        else if (gamepad2.right_trigger > 0.5) {
-            if (gamepad2.a)
-                liftDrive.setTargetPosition(0);
-            else if (gamepad2.b)
-                liftDrive.setTargetPosition(-JUNCTION_ENCODING_SHORT);
-            else if (gamepad2.x)
-                liftDrive.setTargetPosition(-JUNCTION_ENCODING_MEDIUM);
-            else if (gamepad2.y)
-                liftDrive.setTargetPosition(-JUNCTION_ENCODING_TALL);
-            liftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            liftDrive.setPower(LIFT_MOTOR_SPEED);
-        } else {
-            liftDrive.setTargetPosition(liftDrive.getCurrentPosition());
-            liftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        }
-
-        telemetry.addData("Lift",  " at %7d", liftDrive.getCurrentPosition());
-    }
-
-    private void updateClaw() {
-        // First callibrate
-        if (gamepad2.left_bumper) {
-            claw.setPosition(CLAW_POSITION_OPEN);
-            telemetry.addData("Button", "Left bumper");
-            telemetry.update();
-        }
-        else if (gamepad2.right_bumper)
-            claw.setPosition(CLAW_POSITION_CLOSED);
-
-        if (gamepad2.a && gamepad2.right_trigger < 0.5) {
-            wrist.setPosition(WRIST_POSITION_LEVEL);
-        } else if (gamepad2.b && gamepad2.right_trigger < 0.5) {
-            wrist.setPosition(WRIST_POSITION_DOWN);
-        }
-    }
-
-    /*
-    public void encoderDrive(double speed, int encoderTarget, double timeoutS) {
-
-        // Ensure that the opmode is still active
-        if (opModeIsActive()) {
-
-            // Determine new target position, and pass to motor controller
-            liftDrive.setTargetPosition(encoderTarget);
-
-            // Turn On RUN_TO_POSITION
-            liftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            // reset the timeout time and start motion.
-            runtime.reset();
-            liftDrive.setPower(Math.abs(speed));
-
-            // keep looping while we are still active, and there is time left, and both motors are running.
-            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-            // its target position, the motion will stop.  This is "safer" in the event that the robot will
-            // always end the motion as soon as possible.
-            // However, if you require that BOTH motors have finished their moves before the robot continues
-            // onto the next step, use (isBusy() || isBusy()) in the loop test.
-            while (opModeIsActive() &&
-                    (runtime.seconds() < timeoutS) &&
-                    (liftDrive.isBusy())) {
-
-                // Display it for the driver.
-                telemetry.addData("Running to",  " %7d", encoderTarget);
-                telemetry.addData("Currently at",  " at %7d", liftDrive.getCurrentPosition());
-                telemetry.update();
-            }
-
-            // Stop all motion;
-            liftDrive.setPower(0);
-
-            // Turn off RUN_TO_POSITION
-            liftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-            sleep(250);   // optional pause after each move.
-        }
-    }
-    */
  }
 
